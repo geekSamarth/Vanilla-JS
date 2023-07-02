717603b1e2f2a7cf4194ed44b0181b66cf89b471
samarth goyal <samarthagrawal1804@gmail.com>
2023-07-02 23:19:36 +0530

    Basic functioning is added

--- Guess Number Game/main.js
+++ Guess Number Game/main.js
@@ -1,38 +1,44 @@
 const guessField = document.getElementById("guessField");
 const submit = document.querySelector("#btn");
-const chanceLeft = document.querySelector("#chanceleft");
+const chanceLeft = document.querySelector("#chanceLeft");
 const guessValue = document.querySelector("#guessValue");
 const result = document.querySelector("#result");
+
 // function to generate the random number between 1 and 100.
 let randomNumber = Math.floor(Math.random() * 100 + 1);
+var count =10;
+
 
 // function for checking the values.
 function guessNumber() {
   
   console.log(randomNumber);
 
-  var counter = 10;
 
-  if(counter > 0)
+  if(count > 0)
   {
-    while (counter > 0) {
+    while (count > 0) {
         let inputValue = guessField.value;
     
         if (inputValue === randomNumber) {
           result.innerHTML = `YaY!! You won the game`;
-          counter--;
+          count--;
           guessValue.innerHTML = `Equal.`;
-          chanceLeft.innerHTML = `${counter}`;
+          chanceLeft.innerHTML = `${count}`;
           inputValue = "";
         } else if (inputValue >= randomNumber) {
-          counter--;
-          chanceLeft.innerHTML = `${counter}`;
+          count--;
+          chanceLeft.innerHTML = `${count}`;
           guessValue.innerHTML = `High`;
           inputValue = "";
 
         } else {
-          counter--;
-          chanceLeft.innerHTML = `${counter}`;
+          if(count >0)
+          {
+            count--;
+
+          }
+          chanceLeft.innerHTML = `${count}`;
           guessValue.innerHTML = `Low`;
           inputValue = "";
 
