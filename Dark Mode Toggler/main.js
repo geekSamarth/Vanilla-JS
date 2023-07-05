e45e72514b9d0495265754477f8a804acd5bab61
samarth goyal <samarthagrawal1804@gmail.com>
2023-07-05 05:55:38 +0530

    Adding Optimized script

--- Dark Mode Toggler/main.js
+++ Dark Mode Toggler/main.js
@@ -1,20 +1,35 @@
 const content = document.getElementById("content");
 const themeToggler = document.getElementById("theme-toggler");
 
-function themeChanger(){
-    if(themeToggler.src.match('moon'))
-    {
-        themeToggler.src = './sun.png';
-        content.classList.toggle('dark');
+
+// lengthy approach
+
+// function themeChanger(){
+//     if(themeToggler.src.match('moon'))
+//     {
+//         themeToggler.src = './Images/sun.png';
+//         content.classList.toggle('dark');
+//     }
+//     else
+//     {
+//         themeToggler.src='./Images/moon.png';
+//         content.classList.toggle('dark');
+//     }
+// }
+
+// themeToggler.addEventListener('click',function(e){
+//     e.preventDefault();
+//     themeChanger();
+// })
+
+// optimized approach
+
+themeToggler.onclick = function(){
+    content.classList.toggle('dark');
+    if(content.classList.contains('dark')){
+        themeToggler.src = './Images/sun.png';
     }
-    else
-    {
-        themeToggler.src='./moon.png';
-        content.classList.toggle('dark');
+    else{
+        themeToggler.src = './Images/moon.png';
     }
 }
-
-themeToggler.addEventListener('click',function(e){
-    e.preventDefault();
-    themeChanger();
-})
