4d8030ca67efcf705936ab3f660444cf97bb548c
samarth goyal <samarthagrawal1804@gmail.com>
2023-07-10 03:31:18 +0530

    Adding styles and script

--- OTP Generator/main.js
+++ OTP Generator/main.js
@@ -1,1 +1,24 @@
+const trigger = document.querySelector('.trigger');
+const otpNumber = document.querySelector('#otp');
+const otpLength = document.getElementById('otpLength');
+
 //logic for otp generation
+
+function getOtpNumber(){
+    let lengthValue = otpLength.value;
+    console.log(length)
+    let number = '0123456789';
+    let otp = '';
+    for(i=0;i<lengthValue;i++)
+    {
+        otp += number[Math.floor(Math.random()*10)];
+    }
+    otpLength.value='';
+    return otp;
+}
+
+trigger.addEventListener('click',function(e){
+    e.preventDefault();
+    const otp =getOtpNumber();
+    otpNumber.innerHTML = `${otp}`;
+})
